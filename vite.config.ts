64908c55869dd41bf65f62de3bfe1078845f5e00
NovaDesignedIt <meechlamirande2@gmail.com>
2024-03-07 13:10:38 -0500

    pushing changes to staging

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: 'localhost', // Replace with your actual IP address within the 192 network
-    port: 3000, // Or any other port you want to use
+    host: '192.168.2.196', // Replace with your actual IP address within the 192 network
+    port: 9000, // Or any other port you want to use
   },
   plugins: [react()],
 })
